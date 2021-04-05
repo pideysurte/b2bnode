44d1fbdcd3017fefbd617bfedb91e38276ea279f
pideysurte <81972600+pideysurte@users.noreply.github.com>
2021-04-05 09:07:38 -0500

    Add files via upload

---
+++ src/config/config.js
@@ -0,0 +1,15 @@
+export const secret = 'shopliftjmonetaryjupstroke';
+export const serverMail = {
+    host: "smtp-mail.outlook.com", // hostname
+    secure: false, // TLS requires secureConnection to be false
+    port: 587, // port for secure SMTP
+    tls: {
+        rejectUnauthorized: false
+    },
+    auth: {
+        user: "email",
+        pass: "clavecorreo"
+    }
+}
+
+export const saltCodes = 'UrMtNik7zajSLuDY';
