782590220c7d7831af0ac3fc78bdb777df9c8b9c
pideysurte <81972600+pideysurte@users.noreply.github.com>
2021-04-07 08:53:14 -0500

    Add files via upload

---
+++ src/controllers/userComercialCedi.controller.js
@@ -0,0 +1,88 @@
+import {
+    getOne
+} from '../services/userComercialCedi/getOne';
+import {
+    getAll
+}
+from '../services/userComercialCedi/getAll';
+import {
+    getCreate
+}
+from '../services/userComercialCedi/getCreate';
+
+import {
+    getUpdate
+}
+from '../services/userComercialCedi/getUpdate';
+import {
+    getDelete
+}
+from '../services/userComercialCedi/getDelete';
+
+
+import errors from "../utils/codeInternalErrors";
+import ResponseUtil from "../utils/response";
+import DefaultConstants from "../utils/defaultConstants";
+
+import log4js from "log4js";
+const logger = log4js.getLogger();
+logger.level = DefaultConstants.logerlevel;
+
+
+export async function getAllData(req, res, next) {
+    try {
+        logger.info("[getAllData] INIT");
+        let params = await getAll();
+        ResponseUtil.success(res, params);
+    } catch (error) {
+        logger.info("[getAllData]  ERROR", error);
+        ResponseUtil.badRequest(res, errors.DATA_NOT_FOUND, error.message);
+    }
+}
+
+
+export async function getCreateData(req, res, next) {
+    try {
+        logger.info("[getCreateData] INIT");
+
+        let params = await getCreate(req.body, req.files);
+        ResponseUtil.success(res, params);
+    } catch (error) {
+        logger.info("[getCreateData]  ERROR", error);
+        ResponseUtil.badRequest(res, errors.DATA_NOT_FOUND, error.message);
+    }
+}
+
+export async function getOneData(req, res, next) {
+    try {
+        logger.info("[getOneData] INIT");
+        let params = await getOne(req.body);
+        ResponseUtil.success(res, params);
+    } catch (error) {
+        logger.info("[getOneData]  ERROR", error);
+        ResponseUtil.badRequest(res, errors.DATA_NOT_FOUND, error.message);
+    }
+}
+
+export async function getUpdateData(req, res, next) {
+    try {
+        logger.info("[getUpdateData] INIT");
+        console.log(req.body)
+        console.log(req.files)
+        let params = await getUpdate(req.body, req.files);
+        ResponseUtil.success(res, params);
+    } catch (error) {
+        logger.info("[getUpdateData]  ERROR", error);
+        ResponseUtil.badRequest(res, errors.DATA_NOT_FOUND, error.message);
+    }
+}
+export async function getDeleteData(req, res, next) {
+    try {
+        logger.info("[getDeleteData] INIT");
+        let params = await getDelete(req.body);
+        ResponseUtil.success(res, params);
+    } catch (error) {
+        logger.info("[getDeleteData]  ERROR", error);
+        ResponseUtil.badRequest(res, errors.DATA_NOT_FOUND, error.message);
+    }
+}
